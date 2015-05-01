53748d9b9d915ef37da6ddb6473fabd8f061404b
donboyd5 <donboyd5@gmail.com>
2015-05-01 15:41:24 -0400

    update

---
+++ R/fof.r
@@ -0,0 +1,17 @@
+#' fof Flow of funds.
+#'
+#' fof Flow of funds (FOF), ALL data, national, from a recent release.
+#'
+#' @source Federal Reserve Board Z1 release (http://www.federalreserve.gov/releases/z1/current/), download and parse entire html file using library(apitools)
+#' @format Data frame with 1 row per variable per date, NA records have been removed.
+#' \describe{
+#' \item{variable}{FOF variable name, including suffix denoting frequency, factor}
+#' \item{freq}{Frequency of data (A or Q), factor}
+#' \item{description}{Long description of variable, factor}
+#' \item{date}{first day of quarter for Q data, first day of year for A data}
+#' \item{value}{$ amount, in units described in FOF documentation [ADD a units var], numeric}
+#' }
+#' @examples
+#'   head(fof)
+"fof"
+
---
+++ data-raw/fof_FRB_flow_of_funds.r
@@ -0,0 +1,20 @@
+
+library(apitools)
+library(btools)
+library(dplyr)
+
+df <- z1()
+ht(df)
+memory()
+# make the file smaller and easier to work with
+fof <- df %>% mutate(description=factor(description),
+                      freq=factor(freq),
+                      variable=factor(variable),
+                      date=as.Date(date)) %>%
+  filter(!is.na(value))
+glimpse(fof)
+# count(fof, freq)
+# count(fof, is.na(value))
+
+devtools::use_data(fof, overwrite=TRUE)
+
