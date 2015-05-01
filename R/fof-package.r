43f1e5efe6ca75b4e8e6817af90611b191423641
donboyd5 <donboyd5@gmail.com>
2015-05-01 14:49:58 -0400

    Streamlined data

---
+++ R/fof-package.r
@@ -0,0 +1,10 @@
+#' fof.
+#  Keep this file so that a user can type ?fof and get the index for the package.
+#'
+#' @description
+#' Flow of funds - data that I use regularly.
+#'
+#' @name fof
+#' @docType package
+NULL
+
--- inst/fof_FRB_flow_of_funds.r
+++ inst/fof_FRB_flow_of_funds.r
@@ -7,14 +7,14 @@ df <- z1()
 ht(df)
 memory()
 # make the file smaller and easier to work with
-fof <- df %>% mutate(description=factor(description),
+fz1 <- df %>% mutate(description=factor(description),
                       freq=factor(freq),
                       variable=factor(variable),
                       date=as.Date(date)) %>%
   filter(!is.na(value))
-glimpse(fof)
+glimpse(fz1)
 # count(fof, freq)
 # count(fof, is.na(value))
 
-devtools::use_data(fof, overwrite=TRUE)
+devtools::use_data(fz1, overwrite=TRUE)
 
